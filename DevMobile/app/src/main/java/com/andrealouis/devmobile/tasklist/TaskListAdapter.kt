db07f00834bb17aebf717b7faf1e33586f1f8fad
Asbibi <66073617+Asbibi@users.noreply.github.com>
2020-11-23 16:43:50 +0100

    Adding task list<string>

---
+++ DevMobile/app/src/main/java/com/andrealouis/devmobile/tasklist/TaskListAdapter.kt
@@ -0,0 +1,36 @@
+package com.andrealouis.devmobile.tasklist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.andrealouis.devmobile.R
+
+class TaskListAdapter(private val taskList: List<String>) : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
+
+
+    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(taskTitle: String) {
+            itemView.apply { // `apply {}` permet d'éviter de répéter `itemView.*`
+                var textView = itemView.findViewById<TextView>(R.id.task_title)
+                textView.text = taskTitle
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return taskList.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(taskList[position])
+    }
+
+
+}
