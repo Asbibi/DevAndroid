cbb06bfdd6ab82fb558d791ee6fd70cf3994e09d
Asbibi <66073617+Asbibi@users.noreply.github.com>
2020-11-23 18:28:18 +0100

    Adding add button

---
+++ DevMobile/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+k
+;app/src/main/java/com/andrealouis/devmobile/MainActivity.kt,5\d\5df42a0c357dfba79c144b07c6a9889b10a52bb8
+l
+<app/src/main/java/com/andrealouis/devmobile/tasklist/Task.kt,d\6\d6e9f4dc564e8f8deeb0c18b604d8c84e2805ced
+w
+Gapp/src/main/java/com/andrealouis/devmobile/tasklist/TaskListAdapter.kt,e\3\e3aaff989583cc9d14105ad61c4d78b3e3cc2633
+x
+Happ/src/main/java/com/andrealouis/devmobile/tasklist/TaskListFragment.kt,0\4\0481cd7a0ff3dc560a8a724d3f78fc02b63cab01
--- DevMobile/app/src/main/java/com/andrealouis/devmobile/tasklist/TaskListFragment.kt
+++ DevMobile/app/src/main/java/com/andrealouis/devmobile/tasklist/TaskListFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.andrealouis.devmobile.R
+import java.util.*
 
 class TaskListFragment : Fragment() {
 
     //private val taskList = listOf("Task 1", "Task 2", "Task 3")
-    private val taskList = listOf(
+    private val taskList = mutableListOf(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3")
@@ -34,6 +36,13 @@ class TaskListFragment : Fragment() {
         // Pour une [RecyclerView] ayant l'id "recycler_view":
         var recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(activity)
-        recyclerView.adapter = TaskListAdapter(taskList)
+        val taskListAdapter = TaskListAdapter(taskList)
+        recyclerView.adapter = taskListAdapter
+        var button = view.findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            val task = Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList.add(taskList.size, task)
+            taskListAdapter.notifyItemInserted(taskList.size)
+        }
     }
 }
